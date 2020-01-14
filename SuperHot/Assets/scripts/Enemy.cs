a6af41c39b66827bb73c4f5a921f2ea2c3123bd8
26689 <tasso.mevissen@gmail.com>
2020-01-14 10:29:57 +0100

    Enemmy 1.2
    changed movemnt to use navmesh

--- SuperHot/Assets/scripts/Enemy.cs
+++ SuperHot/Assets/scripts/Enemy.cs
@@ -1,54 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject ThePlayer;
-    public float TargetDistance;
-    public float AllowedRange = 10;
-    //public GameObject TheEnemy;
-    public float EnemySpeed;
-    public int AttackTrigger;
-    public RaycastHit shot;
+    GameObject Player;
+    NavMeshAgent enemy;
 
-     void Update()
+
+     void Start()
     {
-        transform.LookAt(ThePlayer.transform);
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out shot))
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+
+        if(Player == null)
         {
-            TargetDistance = shot.distance;
-            if(TargetDistance < AllowedRange)
-            {
-                EnemySpeed = 10f;
-                if(AttackTrigger == 0)
-                {
-                    //TheEnemy.GetComponent<Animation>().Play("Walking");
-                    transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
-                }
-            }
-            else
-            {
-                EnemySpeed = 0;
-                //TheEnemy.GetComponent<Animation>().Play("Idle");
-            }
-        }
-        if (AttackTrigger == 1)
-        {
-            EnemySpeed = 0;
-            //TheEnemy.GetComponent<Animation>().Play("Attacking");
+            Debug.Log("tag player");
         }
 
-        void OnTriggerEnter()
-        {
-            AttackTrigger = 1;
-        }
-
-        void onTriggerExit()
-        {
-            AttackTrigger = 0;
-        }
-
+       enemy = GetComponent<NavMeshAgent>();
     }
-    
+
+    void Update()
+    {
+        if(Player != null)
+        enemy.destination = Player.transform.position;
+    }
+
+    // transform. forward gebruiken met time. delta time
+    //navmesh gebruiken
+    //navmes op component zetten
+    //chek er op zetten over distance 
+    //bullet kan naar emty toe gaan in speler
 }
+
+   
+    
+
+
---
+++ SuperHot/Assets/scripts/Shooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    GameObject target;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+    private float speed = 10f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+
+
+    void Update()
+    {
+
+        if (fireCountdown <= 0)
+        {
+            Shoot();
+            fireCountdown = 9f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+}
